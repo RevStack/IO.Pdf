6461b6def64ee4c9186fd2aadee663b615543c16
Scott <sfrancis@misinteractive.com>
2016-01-12 03:13:38 -0500

    Initial commit

---
+++ RevStack.IO.Pdf/HtmlToPdf/HtmlToPdfContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace RevStack.IO.Pdf
+{
+    public class HtmlToPdfContext
+    {
+        public string ExecutablePath { get; set; } 
+        public string FileName { get; set; }
+        public HtmlToPdfContext(string executablePath)
+        {
+            ExecutablePath = executablePath;
+            FileName= DateTime.Now.ToString("ddMMyyyy_hhmmss") + ".pdf";
+        }
+    }
+}
