dbef45b409fcdaffcd893d07500d181e2e663608
Scott <sfrancis@misinteractive.com>
2016-02-02 08:50:00 -0500

    Code rewrite

--- RevStack.IO.Pdf/HtmlToPdf/HtmlToPdfContext.cs
+++ RevStack.IO.Pdf/HtmlToPdf/HtmlToPdfContext.cs
@@ -1,16 +1,89 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 namespace RevStack.IO.Pdf
 {
     public class HtmlToPdfContext
     {
-        public string ExecutablePath { get; set; } 
+        public string ExecutablePath { get; set; }
         public string FileName { get; set; }
+        public string FilePath { get; set; }
         public HtmlToPdfContext(string executablePath)
         {
             ExecutablePath = executablePath;
-            FileName= DateTime.Now.ToString("ddMMyyyy_hhmmss") + ".pdf";
+            FileName = Guid.NewGuid().ToString() + ".pdf"; //random name for temp disk document
+            FilePath = null;
+        }
+
+        public void GeneratePdf(string url)
+        {
+            string args = "rasterize.js " + url + " " + FileName;
+
+            var p = new Process()
+            {
+                StartInfo =
+                    {
+                        FileName = ExecutablePath + @"\phantomjs",
+                        Arguments = args,
+                        UseShellExecute = false, // needs to be false in order to redirect output
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        RedirectStandardInput = true, // redirect all 3, as it should be all 3 or none
+                        WorkingDirectory = ExecutablePath
+                    }
+            };
+
+            p.Start();
+
+            // read the output here...
+            var output = p.StandardOutput.ReadToEnd();
+            var errorOutput = p.StandardError.ReadToEnd();
+
+            // ...then wait n milliseconds for exit (as after exit, it can't read the output)
+            p.WaitForExit(60000);
+
+            // read the exit code, close process
+            int returnCode = p.ExitCode;
+            p.Close();
+
+            // if 0 or 2, it worked so return path of pdf
+            if ((returnCode == 0) || (returnCode == 2))
+                FilePath = Path.Combine(ExecutablePath, FileName);
+            else
+                throw new Exception(errorOutput);
+        }
+
+        public byte[] PdfStream()
+        {
+            if (FilePath == null)
+            {
+                throw new Exception("Pdf document error: Pdf stream requires a generated Pdf document.");
+            }
+            byte[] bytes = new byte[0];
+            bool fail = true;
+
+            while (fail)
+            {
+                try
+                {
+                    using (FileStream file = new FileStream(FilePath, FileMode.Open, FileAccess.Read))
+                    {
+                        bytes = new byte[file.Length];
+                        file.Read(bytes, 0, (int)file.Length);
+                    }
+
+                    fail = false;
+                }
+                catch
+                {
+                    Thread.Sleep(1000);
+                }
+            }
+
+            File.Delete(FilePath);//delete the document on disk before returning
+            return bytes;
         }
     }
 }
--- RevStack.IO.Pdf/HtmlToPdf/HtmlToPdfRepostory.cs
+++ RevStack.IO.Pdf/HtmlToPdf/HtmlToPdfRepostory.cs
@@ -2,37 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.IO;
-using RevStack.IO;
-using System.Diagnostics;
-using System.Threading;
+
 
 namespace RevStack.IO.Pdf
 {
-    public class HtmlToPdfRepostory : IEntityStreamRepository
+    public class HtmlToPdfRepository : IEntityStreamRepository
     {
         private HtmlToPdfContext _context;
-        public HtmlToPdfRepostory(HtmlToPdfContext context)
+        public HtmlToPdfRepository(HtmlToPdfContext context)
         {
             _context = context;
         }
 
         public byte[] Get(string id)
         {
-
-            new Thread(new ParameterizedThreadStart(x =>
-            {
-                ExecuteCommand(string.Format("{0} rasterize.js {1} {2} \"A4\"", _context.ExecutablePath, id, _context.FileName));
-              
-            })).Start();
-
-            var filePath = Path.Combine(_context.ExecutablePath, _context.FileName);
-
-            var stream = new MemoryStream();
-            byte[] bytes = DoWhile(filePath);
-
-            return bytes;
-
+            _context.GeneratePdf(id);
+            return _context.PdfStream();
         }
 
         public EntityStream Add(EntityStream entity)
@@ -60,48 +45,5 @@ namespace RevStack.IO.Pdf
             throw new NotImplementedException();
         }
 
-        private void ExecuteCommand(string Command)
-        {
-            try
-            {
-                ProcessStartInfo ProcessInfo;
-                Process Process;
-
-                ProcessInfo = new ProcessStartInfo("cmd.exe", "/K " + Command);
-
-                ProcessInfo.CreateNoWindow = true;
-                ProcessInfo.UseShellExecute = false;
-
-                Process = Process.Start(ProcessInfo);
-            }
-            catch { }
-        }
-
-        private byte[] DoWhile(string filePath)
-        {
-            byte[] bytes = new byte[0];
-            bool fail = true;
-
-            while (fail)
-            {
-                try
-                {
-                    using (FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                    {
-                        bytes = new byte[file.Length];
-                        file.Read(bytes, 0, (int)file.Length);
-                    }
-
-                    fail = false;
-                }
-                catch
-                {
-                    Thread.Sleep(1000);
-                }
-            }
-
-            File.Delete(filePath);
-            return bytes;
-        }
     }
 }
